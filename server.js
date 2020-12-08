4ff13549bf4779626aeb9748204d6182eab97803
cylam381 <75677393+cylam381@users.noreply.github.com>
2020-12-08 21:24:30 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const session = require('cookie-session');
+const bodyParser = require('body-parser');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const assert = require('assert');
+const fs = require('fs');
+const formidable = require('express-formidable');
+const mongourl = '';
+const dbName = 'test';
+
+app.set('view engine','ejs');
+
+const SECRETKEY = 'I want to pass COMPS381F';
+
+const users = new Array(
+	{name: 'developer', password: 'developer'},
+	{name: 'guest', password: 'guest'}
+);
+
+app.set('view engine','ejs');
+
+app.use(session({
+  name: 'loginSession',
+  keys: [SECRETKEY]
+}));
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const findDocument = (db, criteria, callback) => {
+    let cursor = db.collection('restaurants').find(criteria);
+    cursor.toArray((err,docs) => {
+        assert.equal(err, null);
+        console.log(`findDocument: ${docs.length}`);
+        callback(docs);
+    });
+}
+
+
+
+const handle_Find = (res, criteria, req) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        findDocument(db,criteria,(docs) => {
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('secrets', {name: req.session.username, nRestaurants: docs.length, restaurants: docs});
+        });
+    });
+}
+
+
+
+
+
+const handle_Details = (res, criteria) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        let DOCID = {};
+        DOCID['_id'] = ObjectID(criteria._id)
+        findDocument(db,DOCID,(docs) => {
+            client.close();
+            console.log("Closed DB connection");
+            res.status(200).render('details', {restaurant: docs[0]});
+
+        });
+    });
+}
+
+
+
+app.get('/', (req,res) => {
+	console.log(req.session);
+	if (!req.session.authenticated) {    // user not logged in!
+        res.redirect('/login');
+	} else {
+        handle_Find(res, req.query, req);   //main page
+		//res.status(200).render('secrets',{name:req.session.username});
+	}
+});
+
+app.get('/details', (req,res) => {
+    handle_Details(res, req.query);
+});
+
+
+app.get('/login', (req,res) => {
+	res.status(200).render('login',{});
+});
+
+app.post('/login', (req,res) => {
+	users.forEach((user) => {
+		if (user.name == req.body.name && user.password == req.body.password) {
+			// correct user name + password
+			// store the following name/value pairs in cookie session
+			req.session.authenticated = true;        // 'authenticated': true
+			req.session.username = req.body.name;	 // 'username': req.body.name		
+		}
+	});
+	res.redirect('/');
+});
+
+app.get('/logout', (req,res) => {
+	req.session = null;   // clear cookie-session
+	res.redirect('/');
+});
+
+app.listen(process.env.PORT || 8099);
