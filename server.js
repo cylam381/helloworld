891f95d4e7c7d984ff155f200ac3937190f4d086
cylam381 <75677393+cylam381@users.noreply.github.com>
2020-12-20 00:39:44 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
+const formidable = require('express-formidable');
+const app = express();
 const session = require('cookie-session');
 const bodyParser = require('body-parser');
-const app = express();
+
 
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 const assert = require('assert');
-const fs = require('fs');
-const formidable = require('express-formidable');
+const ExpressFormidable = require('express-formidable');
 const mongourl = '';
 const dbName = 'test';
 
+
 app.set('view engine','ejs');
+
 
 const SECRETKEY = 'I want to pass COMPS381F';
 
 const users = new Array(
-	{name: 'developer', password: 'developer'},
-	{name: 'guest', password: 'guest'}
+	{name: 'demo', password: ''},
+	{name: 'student', password: ''}
 );
-
-app.set('view engine','ejs');
 
 app.use(session({
   name: 'loginSession',
@@ -32,6 +33,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+
+
+
 const findDocument = (db, criteria, callback) => {
     let cursor = db.collection('restaurants').find(criteria);
     cursor.toArray((err,docs) => {
@@ -40,8 +44,6 @@ const findDocument = (db, criteria, callback) => {
         callback(docs);
     });
 }
-
-
 
 const handle_Find = (res, criteria, req) => {
     const client = new MongoClient(mongourl);
@@ -57,10 +59,6 @@ const handle_Find = (res, criteria, req) => {
         });
     });
 }
-
-
-
-
 
 const handle_Details = (res, criteria) => {
     const client = new MongoClient(mongourl);
@@ -80,6 +78,99 @@ const handle_Details = (res, criteria) => {
     });
 }
 
+const uploadDocument = (uploadDoc, callback) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        db.collection('restaurants').insertOne(uploadDoc,(err,results) => {
+            client.close();
+            assert.equal(null, err);
+            callback();
+        });
+    });
+
+}
+
+
+const handle_Create = (req, res, criteria) => {
+ 
+    var uploadDoc = {};
+    uploadDoc['restaurant_id'] = req.fields.restaurant_id;
+    uploadDoc['name'] = req.fields.name;
+    uploadDoc['borough'] = req.fields.borough;
+    uploadDoc['cuisine'] = req.fields.cuisine;
+    uploadDoc['street'] = req.fields.street;
+    uploadDoc['building'] = req.fields.building;
+    uploadDoc['zipcode'] = req.fields.zipcode;
+    uploadDoc['coord']= [req.fields.lon, req.fields.lat];
+    uploadDoc['owner'] = req.fields.owner;
+    uploadDocument(uploadDoc, () => {
+        res.status(200).render('info', {});
+    });
+}
+
+const handle_Edit = (res,criteria) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null,err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        let DOCID = {};
+        DOCID['_id'] = ObjectID(criteria._id);
+
+        let cursor = db.collection('restaurants').find(DOCID);
+        cursor.toArray((err,docs) => {
+            client.close();
+            assert.equal(null, err);
+            res.status(200).render('edit', {restaurant: docs[0]});
+
+        });
+    });
+}
+
+const updateDocument = (criteria, updateDoc, callback) => {
+    const client = new MongoClient(mongourl);
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const db = client.db(dbName);
+
+        db.collection('restaurants').updateOne(criteria,
+            {
+                $set : updateDoc
+            },
+            (err, results) => {
+                client.close();
+                assert.equal(null, err);
+                callback(results);
+            }
+        );
+    });
+}
+
+const handle_Update = (req, res, criteria) => {
+    var DOCID = {};
+    DOCID['_id'] = ObjectID(req.fields._id);
+    
+    var updateDoc = {};
+    updateDoc['restaurant_id'] = req.fields.restaurant_id;
+    updateDoc['name'] = req.fields.name;
+    updateDoc['borough'] = req.fields.borough;
+    updateDoc['cuisine'] = req.fields.cuisine;
+    updateDoc['street'] = req.fields.street;
+    updateDoc['building'] = req.fields.building;
+    updateDoc['zipcode'] = req.fields.zipcode;
+    updateDoc['coord']= [req.fields.lon, req.fields.lat];
+    updateDoc['owner'] = req.fields.owner;
+
+    updateDocument(DOCID, updateDoc, (results) => {
+        res.status(200).render('update', {message: `Updated ${results.result.nModified} document(s)`});
+    });
+}
 
 
 app.get('/', (req,res) => {
@@ -91,11 +182,6 @@ app.get('/', (req,res) => {
 		//res.status(200).render('secrets',{name:req.session.username});
 	}
 });
-
-app.get('/details', (req,res) => {
-    handle_Details(res, req.query);
-});
-
 
 app.get('/login', (req,res) => {
 	res.status(200).render('login',{});
@@ -117,5 +203,31 @@ app.get('/logout', (req,res) => {
 	req.session = null;   // clear cookie-session
 	res.redirect('/');
 });
+
+app.get('/create', (req,res) => {
+    res.status(200).render('create',{owner:req.session.username});
+});
+
+app.use('/create',formidable());
+
+app.post('/create', (req,res) => {
+    console.log("hello");
+    handle_Create(req,res,req.query);
+});
+
+app.get('/details', (req,res) => {
+    handle_Details(res,req.query);
+});
+
+app.get('/edit', (req,res) => {
+    handle_Edit(res,req.query);
+});
+
+app.use('/update',formidable());
+
+app.post('/update', (req,res) => {
+    handle_Update(req,res,req.query);
+});
+
 
 app.listen(process.env.PORT || 8099);
